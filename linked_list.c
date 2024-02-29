86c17179d81b6fd1a5666a5acd9dc22d49a5a231
moldypeaches <adelejulietmartin@gmail.com>
2024-02-29 11:01:55 +0100

    ok

--- linked_list.c
+++ linked_list.c
@@ -6,7 +6,7 @@
 /*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:27:54 by ademarti          #+#    #+#             */
-/*   Updated: 2024/02/28 14:04:37 by ademarti         ###   ########.fr       */
+/*   Updated: 2024/02/29 11:01:36 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,34 +26,35 @@ void append_stack(s_stack *s_a, int n)
 	new_node = malloc(sizeof(s_stack));
 	if (!new_node)
 		exit(EXIT_FAILURE);
-	s_a->nb = n;
-	s_a->next = NULL;
+	new_node->nb = n;
+	new_node->next = s_a->next;
+	s_a->next = new_node;
 }
 
 int main (int ac, char **av)
 {
 	int j;
 	s_stack s_a;
-	s_a.nb = -1;
-	s_a.next = malloc(sizeof(s_stack));
+	s_a.next = NULL;
 	s_stack *curr;
 	curr = &s_a;
 
-	j = 0;
+	j = 1;
 
 //Change this to ft_atoi asaaaap
 	while (av[j])
 	{
 		append_stack(&s_a, atoi(av[j]));
-		printf("%d", curr->nb);
-		curr = curr->next;
 		j++;
-
 	}
+	while (curr->next != NULL)
+		{
+        curr = curr->next;
+		printf("%d ", curr->nb);
+    	}
 
 	s_stack s_b;
-	//s_a.next->nb = -1;
-	//s_a.next->next = NULL;
+	s_a.next->next = NULL;
 	s_b.nb = -1;
     s_b.next = NULL;
 	free(s_a.next);
