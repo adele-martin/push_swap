6adf7383b219e21a62c8934b76936d4cbd376fae
Adele Martin <ademarti@c1b2c3.42berlin.de>
2024-03-05 12:58:05 +0100

    ok

--- main.c
+++ main.c
@@ -6,18 +6,11 @@
 /*   By: ademarti <ademarti@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:27:54 by ademarti          #+#    #+#             */
-/*   Updated: 2024/03/05 12:25:05 by ademarti         ###   ########.fr       */
+/*   Updated: 2024/03/05 12:57:54 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-typedef struct s_stack
-{
-	int nb;
-	struct s_stack *next;
-	struct s_stack *prev;
-} s_stack;
 
 void insertFront(s_stack **s_a, int n)
 {
@@ -29,30 +22,28 @@ void insertFront(s_stack **s_a, int n)
 	new_node->nb = n;
 	new_node->next = (*s_a);
 	new_node->prev = NULL;
-
 	if ((*s_a) != NULL)
 		(*s_a)->prev = new_node;
-
-	s_a = new_node;
+	s_a = &new_node;
 }
 
 int	main (int ac, char **av)
 {
-	s_stack *head;
-	s_stack	s_a;
-	s_stack	s_b;
+	s_stack	*s_a;
+	s_stack	*s_b;
 	int		j;
 	int		i;
 	s_stack	*curr;
 
-	s_a.next = NULL;
+	s_a = NULL;
+	s_b = NULL;
 	j = 1;
 	i = 0;
 	while (av[j])
 	{
 		if (ac <= 2)
 		{
-			return;
+			return 0;
 		}
 		i = 0;
 		while (av[j][i])
@@ -61,7 +52,7 @@ int	main (int ac, char **av)
 		(!((av[j][i] >= '0' && av[j][i] <= '9') || av[j][i] == '+' || av[j][i] == '-' || av[j][i] == ' ')))
 			{
 			ft_putstr_fd("Error\n", 1);
-			return;
+			return 0;
 			}
 		i++;
 		}
@@ -70,20 +61,13 @@ int	main (int ac, char **av)
 	j = 1;
 	while (av[j])
 	{
-		append_stack(&s_a, ft_atoi(av[j]));
+		insertFront(&s_a, ft_atoi(av[j]));
 		j++;
 	}
-	// curr = &s_a;
-	// while (curr->next != NULL)
-	// 	{
-	// 	curr = curr->next;
-	// 		printf("%d ", curr->nb);
-	// 	}
-	s_b.nb = -1;
-	s_b.next = NULL;
-	pa(&s_a, &s_b);
 	printf("hey");
+	// pa(&s_a, &s_b);
 
-	free(s_a.next);
-	free(s_b.next);
+	// free(s_a.next);
+	// free(s_b.next);
+	return 0;
 }
